569c82681e566152d2de2de551bc6eb540d4ebbe
Zachary <zbberman@yahoo.com>
2019-02-06 15:52:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,69 @@ def monopoly_with_third_tier
 
 end
 
+# describe "adding key-value pairs to the third level of the monopoly hash that we continued building in the #monopoly_with_second_tier method" do
+#     describe "#monopoly_with_third_tier" do
+#       it "sets the 1st key of the :rent_in_dollars hash to a symbol, :one_piece_owned, whose value is the integer 25" do
+#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:rent_in_dollars].values[0]).to eq(25)
+#         expect(monopoly_with_third_tier.values[0][:rent_in_dollars].keys[0]).to eq(:one_piece_owned)
+#       end
+
+#       it "sets the 2nd key of the :rent_in_dollars hash to a symbol, :two_pieces_owned, whose value is the integer 50" do
+
+#         expect(monopoly_with_third_tier.keys.count).to eq(1)
+#         expect(monopoly_with_third_tier.values[0].keys.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:rent_in_dollars][:two_pieces_owned]).to eq(50)
+#       end
+
+#       it "sets the 3rd key of the :rent_in_dollars hash to a symbol, :three_pieces_owned, whose value is the integer 100" do
+
+#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:rent_in_dollars].keys[0]).to eq(:one_piece_owned)
+#         expect(monopoly_with_third_tier.values[0][:rent_in_dollars][:three_pieces_owned]).to eq(100)
+#       end
+
+#       it "sets the 4th key of the :rent_in_dollars hash to a symbol, :four_pieces_owned, whose value is the integer 200" do
+
+#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:rent_in_dollars].keys.count).to eq(4)
+#         expect(monopoly_with_third_tier.values[0][:rent_in_dollars][:four_pieces_owned]).to eq(200)
+#       end
+
+#       it "sets the 1st key of the :names hash to a symbol, :reading_railroad, whose value is an empty hash" do
+
+#         expect(monopoly_with_third_tier.keys.count).to eq(1)
+#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:names].keys[0]).to eq(:reading_railroad)
+#         expect(monopoly_with_third_tier.values[0][:names].values[0]).to eq({})
+#       end
+
+#       it "sets the 2nd key of the :names hash to a symbol, :pennsylvania_railroad, whose value is an empty hash" do
+
+#         expect(monopoly_with_third_tier.keys.count).to eq(1)
+#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:names].keys[1]).to eq(:pennsylvania_railroad)
+#         expect(monopoly_with_third_tier.values[0][:names].values[1]).to eq({})
+#       end
+
+#       it "sets the 3rd key of the :names hash to a symbol, :b_and_o_railroad, whose value is an empty hash" do
+
+#         expect(monopoly_with_third_tier.keys.count).to eq(1)
+#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:names].keys[2]).to eq(:b_and_o_railroad)
+#         expect(monopoly_with_third_tier.values[0][:names].values[2]).to eq({})
+#       end
+
+#       it "sets the 4th key of the :names hash to a symbol, :shortline, whose value is an empty hash" do
+
+#         expect(monopoly_with_third_tier.keys.count).to eq(1)
+#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
+#         expect(monopoly_with_third_tier.values[0][:names].keys[3]).to eq(:shortline)
+#         expect(monopoly_with_third_tier.values[0][:names].values[3]).to eq({})
+#       end
+#     end
+#   end
+
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
