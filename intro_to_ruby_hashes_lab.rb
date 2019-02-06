33494ab4df6ce805d6d15d8a6982354023c3546d
Zachary <zbberman@yahoo.com>
2019-02-06 15:55:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,42 +45,6 @@ def monopoly_with_third_tier
 	monopoly
 end
 
-# describe "adding key-value pairs to the third level of the monopoly hash that we continued building in the 
-
-#       it "sets the 1st key of the :names hash to a symbol, :reading_railroad, whose value is an empty hash" do
-
-#         expect(monopoly_with_third_tier.keys.count).to eq(1)
-#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-#         expect(monopoly_with_third_tier.values[0][:names].keys[0]).to eq(:reading_railroad)
-#         expect(monopoly_with_third_tier.values[0][:names].values[0]).to eq({})
-#       end
-
-#       it "sets the 2nd key of the :names hash to a symbol, :pennsylvania_railroad, whose value is an empty hash" do
-
-#         expect(monopoly_with_third_tier.keys.count).to eq(1)
-#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-#         expect(monopoly_with_third_tier.values[0][:names].keys[1]).to eq(:pennsylvania_railroad)
-#         expect(monopoly_with_third_tier.values[0][:names].values[1]).to eq({})
-#       end
-
-#       it "sets the 3rd key of the :names hash to a symbol, :b_and_o_railroad, whose value is an empty hash" do
-
-#         expect(monopoly_with_third_tier.keys.count).to eq(1)
-#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-#         expect(monopoly_with_third_tier.values[0][:names].keys[2]).to eq(:b_and_o_railroad)
-#         expect(monopoly_with_third_tier.values[0][:names].values[2]).to eq({})
-#       end
-
-#       it "sets the 4th key of the :names hash to a symbol, :shortline, whose value is an empty hash" do
-
-#         expect(monopoly_with_third_tier.keys.count).to eq(1)
-#         expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-#         expect(monopoly_with_third_tier.values[0][:names].keys[3]).to eq(:shortline)
-#         expect(monopoly_with_third_tier.values[0][:names].values[3]).to eq({})
-#       end
-#     end
-#   end
-
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
