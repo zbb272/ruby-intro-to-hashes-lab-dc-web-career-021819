cb93106cad79110339c5631c1bdf026e414a83af
Zachary <zbberman@yahoo.com>
2019-02-06 15:58:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -66,6 +66,7 @@ def monopoly_with_fourth_tier
 	monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
 	monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
 	monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+	monopoly[:railroads][:names][:shortline]["mortgage_value"] = "$800"
 	
 	monopoly
 end
